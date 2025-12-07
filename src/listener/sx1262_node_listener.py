4a2659c20d2150c0f5d1aafb82f3b52318c96d1c
glebovitz <gregg@lebovitz.net>
2025-12-07 17:10:27 -0500

    debugging

--- src/listener/sx1262_node_listener.py
+++ src/listener/sx1262_node_listener.py
@@ -3,10 +3,13 @@ from listener.node_listener import NodeListener
 from sx1262.sx1262 import SX1262
 
 class SX1262NodeListener(NodeListener):
-    def __init__(self, port="/dev/ttyS0", baudrate=9600,
-                 contact_store=None, message_store=None):
-        super().__init__(contact_store,
-                         message_store)
+    def __init__(self, 
+                 port="/dev/ttyS0", 
+                 baudrate=9600,
+                 contact_store=None, 
+                 message_store=None):
+        # super().__init__(contact_store=contact_store,
+        #                  message_store=message_store)
         self.port = port
         self.baudrate = baudrate
         self.radio = SX1262(serial_port=port, baudrate=baudrate)
