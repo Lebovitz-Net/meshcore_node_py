52dea9d1ee497d10979f0a39a9a94378ce89e89a
glebovitz <gregg@lebovitz.net>
2025-12-08 17:24:16 -0500

    debug

--- src/sx1262/sx1262.py
+++ src/sx1262/sx1262.py
@@ -8,21 +8,21 @@ from .sx1262_config import SX1262Config
 from .sx1262_mode import SX1262Mode
 from .sx1262_status import SX1262Status
 
-CS_PIN   = 21   # Chip select
-RST_PIN  = 18   # Reset
-BUSY_PIN = 20   # Busy line
-DIO1_PIN = 16   # Interrupt (RX/TX done)
+# CS_PIN   = 21   # Chip select
+# RST_PIN  = 18   # Reset
+# BUSY_PIN = 20   # Busy line
+# DIO1_PIN = 16   # Interrupt (RX/TX done)
 
-# CS_PIN   = 8    # CE0
-# RST_PIN  = 22
-# BUSY_PIN = 23
-# DIO1_PIN = 24
+CS_PIN   = 8    # CE0
+RST_PIN  = 22
+BUSY_PIN = 23
+DIO1_PIN = 24
 
 class SX1262(SX1262Buffer, SX1262Config, SX1262Mode, SX1262Status):
     def __init__(self, spi_bus=0, spi_dev=0, max_speed=500000):
         # Setup GPIO
         GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
+        # GPIO.setwarnings(False)
         GPIO.setup(RST_PIN, GPIO.OUT)
         GPIO.setup(BUSY_PIN, GPIO.IN)
         GPIO.setup(DIO1_PIN, GPIO.IN)
